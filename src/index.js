2cb9aefd9f645bf1e43ea03fc69b3daa77ea8361
astragenius <darthmalak86@gmail.com>
2022-08-28 20:04:50 +0200

    create - symbol toggle switch component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './scss/index.scss'
 import './assets/img/icon-restart.svg'
+import x from './assets/img/toggle-x.svg'
+import O from './assets/img/toggle-O.svg'
+
+const imgX = document.querySelector('.toggle-symbol-X')
+const imgO = document.querySelector('.toggle-symbol-O')
+
+imgX.src = x
+imgO.src = O
