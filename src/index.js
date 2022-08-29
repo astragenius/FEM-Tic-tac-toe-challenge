51c3c97d7700e818c48160ab495bf5c897ac4f97
astragenius <darthmalak86@gmail.com>
2022-08-29 20:20:59 +0200

    style - added inline svg with viebox attribute

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ import O from './assets/img/toggle-O.svg'
 const imgX = document.querySelector('.toggle-symbol-X')
 const imgO = document.querySelector('.toggle-symbol-O')
 
-imgX.src = x
-imgO.src = O
+const toggle = document.querySelector('#toggle')
+
+console.log(toggle.checked)
