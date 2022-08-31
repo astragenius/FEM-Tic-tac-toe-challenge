aa4a363d21e880161b5d8a6dfc4092aa2188b9f5
astragenius <darthmalak86@gmail.com>
2022-08-31 22:10:53 +0200

    style - add grid game board

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './scss/index.scss'
 import './assets/img/icon-restart.svg'
 import logoImg from './assets/img/logo.svg'
+import xSymbol from './assets/img/toggle-x.svg'
 
 const logo = document.querySelector('.game-menu-logo')
+const headerTurn = document.querySelector('.player-turn-symbol')
+const gameBoardLogo = document.querySelector('.game-board-logo')
 
-logo.src = logoImg
+//logo.src = logoImg
+headerTurn.src = xSymbol
+gameBoardLogo.src = logoImg
