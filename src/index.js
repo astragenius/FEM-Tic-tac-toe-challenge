fe246af55be14f8d3e7f7d836949e3cbaff599c9
astragenius <darthmalak86@gmail.com>
2022-09-02 20:43:49 +0200

    add - added new Javascript file Modal.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import xSymbol from './assets/img/toggle-x.svg'
 const logo = document.querySelector('.game-menu-logo')
 const headerTurn = document.querySelector('.player-turn-symbol')
 const gameBoardLogo = document.querySelector('.game-board-logo')
+const btnReload = document.querySelector('.btn-reload')
+
+btnReload.addEventListener('click', () => {
+    console.log('test')
+})
 
 //logo.src = logoImg
 headerTurn.src = xSymbol
