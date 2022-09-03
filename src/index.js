fd394bd9e4c2d5728aa3b10e16bd252f4e127f5a
astragenius <darthmalak86@gmail.com>
2022-09-03 15:37:14 +0200

    feature - add function to render Modal

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import './scss/index.scss'
 import './assets/img/icon-restart.svg'
 import logoImg from './assets/img/logo.svg'
 import xSymbol from './assets/img/toggle-x.svg'
+import { RenderModal } from './js/Modal'
 
 const logo = document.querySelector('.game-menu-logo')
 const headerTurn = document.querySelector('.player-turn-symbol')
 const gameBoardLogo = document.querySelector('.game-board-logo')
 const btnReload = document.querySelector('.btn-reload')
+const modal = document.querySelector('.modal-container')
 
 btnReload.addEventListener('click', () => {
-    console.log('test')
+    console.log(modal.attributes[0])
+    modal.innerHTML = RenderModal()
+
+    if (modal.getAttribute('data-modal-active') === 'false') {
+        modal.setAttribute('data-modal-active', true)
+    }
 })
 
 //logo.src = logoImg
