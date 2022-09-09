70f043e902a18fb6bbf2c803e4d0fe76e73efa96
astragenius <darthmalak86@gmail.com>
2022-09-09 21:10:58 +0200

    add - method in GameBoard to add eventListeners to the GameBoard tiles

--- src/index.js
+++ src/index.js
@@ -46,38 +46,9 @@ export function update() {
     document.querySelector('.modal-container').innerHTML = newModal.render()
 }
 
-const newModal = new Modal(dataRestart)
-
-/* btnReload.addEventListener('click', () => {
-    if (modal.getAttribute('data-modal-active') === 'false') {
-        modal.setAttribute('data-modal-active', true)
-        modal.innerHTML = newModal.render()
-        newModal.addListener()
-    }
-}) */
-
 //logo.src = logoImg
 headerTurn.src = xSymbol
 gameBoardLogo.src = logoImg
-
-/* const gameField = document.querySelector('.game-field')
-const turnImg = document.querySelector('.player-turn-symbol')
-const tiles = [...document.querySelectorAll('.game-tile')]
-tiles.forEach((tile) => {
-    tile.addEventListener('click', function () {
-        if (gameField.getAttribute('data-turn') === 'X') {
-            turnImg.setAttribute('data-turn', 'x')
-            this.classList.add('x-marker')
-            gameField.setAttribute('data-turn', 'O')
-            headerTurn.src = OSymbol
-        } else {
-            turnImg.setAttribute('data-turn', 'o')
-            this.classList.add('o-marker')
-            gameField.setAttribute('data-turn', 'X')
-            headerTurn.src = xSymbol
-        }
-    })
-}) */
 
 let p1 = new CreatePlayer('X', 'Player1')
 let p2 = new CreatePlayer('O', 'Player2')
--- src/js/GameBoard.js
+++ src/js/GameBoard.js
@@ -1,3 +1,15 @@
+import xSymbol from '../assets/img/toggle-x.svg'
+import OSymbol from '../assets/img/toggle-o.svg'
+import { Modal } from './Modal'
+
+let dataRestart = {
+    playerText: '',
+    headText: 'restart game?',
+    playerSymbol: 'neutral',
+    btnText1: 'no, cancel',
+    btnText2: 'yes, restart',
+}
+
 export class GameBoard {
     constructor(p1, p2) {
         this.p1 = {
@@ -19,6 +31,42 @@ export class GameBoard {
         const p1Score = document.querySelector('#playerScore')
         const tieScore = document.querySelector('#tieScore')
         const p2Score = document.querySelector('#player2Score')
+        this.setEventListener()
+        this.initReloadBtn()
+    }
+
+    setEventListener() {
+        const gameField = document.querySelector('.game-field')
+        const tiles = [...document.querySelectorAll('.game-tile')]
+        const turnImg = document.querySelector('.player-turn-symbol')
+        tiles.forEach((tile) => {
+            tile.addEventListener('click', function () {
+                if (gameField.getAttribute('data-turn') === 'X') {
+                    turnImg.setAttribute('data-turn', 'X')
+                    this.classList.add('x-marker')
+                    gameField.setAttribute('data-turn', 'O')
+                    turnImg.src = OSymbol
+                } else {
+                    turnImg.setAttribute('data-turn', 'O')
+                    this.classList.add('o-marker')
+                    gameField.setAttribute('data-turn', 'X')
+                    turnImg.src = xSymbol
+                }
+            })
+        })
+    }
+
+    initReloadBtn() {
+        const modal = document.querySelector('.modal-container')
+        const btnReload = document.querySelector('.btn-reload')
+        const newModal = new Modal(dataRestart)
+        btnReload.addEventListener('click', function () {
+            if (modal.getAttribute('data-modal-active') === 'false') {
+                modal.setAttribute('data-modal-active', true)
+                modal.innerHTML = newModal.render()
+                newModal.addListener()
+            }
+        })
     }
 
     setPoints(prop) {
