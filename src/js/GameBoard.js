bfa84c9ecb4d89df7bf552a105aa90f7d52f2266
astragenius <darthmalak86@gmail.com>
2022-09-12 21:39:41 +0200

    add - added to the checkwinner function winning combination array

--- src/js/GameBoard.js
+++ src/js/GameBoard.js
@@ -91,6 +91,26 @@ export class GameBoard {
         })
     }
 
+    checkWinner(currendSymbol) {
+        const tiles = [...document.querySelectorAll('.game-tile')]
+        const WINING_COMBINATION = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+
+        return WINING_COMBINATION.some((combination) => {
+            return combination.every((index) => {
+                return tiles[index].classList.contains(currendSymbol)
+            })
+        })
+    }
+
     initReloadBtn() {
         const btnReload = document.querySelector('.btn-reload')
 
