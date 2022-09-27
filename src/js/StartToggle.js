f72e0ceb6aff457e3b3467cd83711c9da5d61157
astragenius <darthmalak86@gmail.com>
2022-09-27 20:17:32 +0200

    del - del input variable on StartToggle

--- src/js/StartToggle.js
+++ src/js/StartToggle.js
@@ -2,7 +2,6 @@ import logoIMG from '../assets/img/logo.svg'
 import { Player, CPU } from './CreatePlayer'
 
 export const StartToggle = (function () {
-    const input = document.querySelector('.checkbox').checked
     const logo = document.querySelector('.game-menu-logo')
     logo.src = logoIMG
 })()
